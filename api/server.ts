6e877780546ffd863f60d683726aa557fbfb0294
iambalaam <guy.balaam@gmail.com>
2022-01-23 12:01:47 +0000

    Adding local overrides for environment variables

--- api/server.ts
+++ api/server.ts
@@ -1,5 +1,13 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { createClient } from "https://denopkg.com/chiefbiiko/dynamodb/mod.ts";
+import { accessKeyId, region, secretAccessKey } from "./credentials.ts";
 
-await serve((_req) => {
+const denamo = createClient({
+  credentials: { accessKeyId, secretAccessKey },
+  region,
+});
+
+await serve(async (_req) => {
+  console.log(await denamo.listTables());
   return new Response("hello noobs");
 });
