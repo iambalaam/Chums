6aac16d1a0e6d97b24994af76e8b5217745ba26e
Guy Balaam <guy@Pimlocs-MacBook-Pro.local>
2023-10-08 16:50:39 +0100

    Fresh deno project

--- web/src/components/game-week-header.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import './game-week-header.css';
-
-export function GameWeekHeader({ week }: { week: string; }) {
-    return <h1 className="game-week">Game Week <span className="number">{week}</span></h1>;
-}
